11a955429e94459798efebfdc9e05591b2562f73
PikkaStar <yokotaka1226@gmail.com>
2023-11-16 07:02:35 +0000

    session end

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,12 +2,15 @@
 
 class Public::SessionsController < Devise::SessionsController
    before_action :configure_sign_in_params, only: [:create]
+   before_action :customer_state,only: [:create]
 
   # GET /resource/sign_in
   # def new
   #   super
   # end
-
+  def after_sign_in_path_for(resource)
+      root_path
+  end
   # POST /resource/sign_in
   # def create
   #   super
@@ -17,6 +20,15 @@ class Public::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+  private
+  def customer_state
+   customer = Customer.find_by(email: params[:customer][:email])
+   return if customer.nil?
+   return unless customer.valid_password?(params[:customer][:password])
+   unless customer.is_active == true
+      redirect_to new_customer_registration_path
+   end
+  end
 
    protected
 
