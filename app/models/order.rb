26b6b4ccd53870148667020d06d929c35ca012f0
EC2 Default User <piyopiyoblue5@gmail.com>
2023-11-16 01:28:44 +0000

    enum-install

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,9 @@
 class Order < ApplicationRecord
 
+
+
+
+
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum order_status: { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparation_for_shipping: 3, sent: 4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,9 @@
 class OrderDetail < ApplicationRecord
 
+
+
+
+
   enum making_status: { unable_to_start: 0, waiting_for_production: 1, in_production: 2, production_completed: 3 }
 
 end
