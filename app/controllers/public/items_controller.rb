dfceb2785c46dec52cc5da12c1497074c69ab8f8
EC2 Default User <piyopiyoblue5@gmail.com>
2023-11-17 00:55:37 +0000

    ページネーション作業中

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,17 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.page(params[:page]).per(8)
     @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
   end
-  
+
   private
-  
+
   def item_params
     params.require(:item).permit(:name, :image, :description, :genre_id, :excluding_tax_price, :is_active)
   end
-  
+
 end
