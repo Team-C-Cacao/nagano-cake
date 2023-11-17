5ca81664c2a870f5741782bfab55effc24034140
EC2 Default User <piyopiyoblue5@gmail.com>
2023-11-17 04:54:42 +0000

    会員側注文実装開始

--- app/controllers/public/oders_controller.rb
+++ app/controllers/public/oders_controller.rb
@@ -3,8 +3,26 @@ class Public::OdersController < ApplicationController
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
+
+  def create
+  end
+
+  def confirm
+  end
+
+  def complete
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:shipping, :payment_method, :total, :order_status, :name, :postal_code, :address)
+  end
+
 end
