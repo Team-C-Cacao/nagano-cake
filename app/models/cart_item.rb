0065805b0470dac1e4727af364b50c7ca45505e2
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-18 17:04:04 +0000

    カート機能実装完了/UIはまた後で調整

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,18 @@
 class CartItem < ApplicationRecord
-  
+
   belongs_to :customer
   belongs_to :item
-  
+
+  def tax_included_price
+    item.excluding_tax_price * 1.10
+  end
+
+  def subtotal
+    tax_included_price * amount
+  end
+
+  def total_price
+    cart_items.sum(&:subtotal)
+  end
+
 end
