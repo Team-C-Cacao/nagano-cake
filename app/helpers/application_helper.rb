655f3d155dc604f954ade586687480d7e675a654
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-19 07:37:44 +0000

    flash実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
-  def bootstrap_class_for(flash_type)
-    case flash_type
-    when 'success'
-      'alert-success'
-    when 'error'
-      'alert-danger'
-    when 'alert'
-      'alert-warning'
-    when 'notice'
-      'alert-info'
+  def bootstrap_class_for_flash(flash_type)
+    case flash_type.to_sym
+    when :success
+      "success"
+    when :error
+      "danger"
+    when :alert
+      "warning"
+    when :notice
+      "info"
     else
       flash_type.to_s
     end
