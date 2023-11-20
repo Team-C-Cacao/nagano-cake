122ab0c5c7f63b6f3f10a8e704eb21c534bda87c
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2023-11-20 01:09:08 +0000

    カラム名編集

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,5 +1,8 @@
 class ShippingAddress < ApplicationRecord
-  
+
   belongs_to :customer
-  
+  validates :name, presence: true
+  validates :shipping_address, presence: true
+  validates :postal_code, presence: true
+
 end
