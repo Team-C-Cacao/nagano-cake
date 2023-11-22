476683b1f2d3e8af863459e95fdb6dff27803198
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2023-11-22 00:03:33 +0000

    一旦

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,15 +1,22 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
   end
   
-  # def update
-    #@order = order.find(params[:id])
-    #if @order.update(order_params)
-     # redirect_to order_path(@order), notice: "注文ステータスの変更しました"
-    #else
-     # render :edit
-    #end
-  #end
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    if @order.update(order_params)
+    end   
+     redirect_to admin_order_path(@order), notice: "注文ステータスの変更しました"
+  end
+  
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:order_status,:making_status)
+  end
   
 end
