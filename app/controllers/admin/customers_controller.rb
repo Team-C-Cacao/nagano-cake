3c0d9f2ac261245f28f1964d9c7c70d670f3f71c
PikkaStar <yokotaka1226@gmail.com>
2023-11-22 13:30:29 +0000

    記述の意味(コントローラ)

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,27 +1,27 @@
 class Admin::CustomersController < ApplicationController
   def index
-    @customers = Customer.all.page(params[:page]).per(10)
+    @customers = Customer.page(params[:page]).per(10)#customerデータを全取得し、1ページ毎にに10件のデータを表示する
     @range = "Customer"
   end
 
   def show
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])#customerモデルから1件のデータを取得
     @range = "Customer"
   end
 
   def edit
-    @customer = Customer.find(params[:id])
-    @user = @customer
+    @customer = Customer.find(params[:id])#customerモデルから1件のデータを取得
+    @user = @customer#編集中に名前でエラーが起こった時にエラーなのに記述内容が更新される不具合を解消するための記述
     @range = "Customer"
   end
 
   def update
-    @customer = Customer.find(params[:id])
+    @customer = Customer.find(params[:id])#customerモデルから1件のデータを取得
     if @customer.update(customer_params)
       flash[:notice] = "登録情報を編集しました"
     redirect_to admin_customer_path(@customer)
     else
-    @user = Customer.find(params[:id])
+    @user = Customer.find(params[:id])#編集中に名前でエラーが起こった時にエラーなのに記述内容が更新される不具合を解消するための記述
     flash.now[:alert] = "登録情報の編集に失敗しました"
     render :edit
     end
@@ -29,6 +29,7 @@ class Admin::CustomersController < ApplicationController
 
   private
 
+  #updateするときのカラムの許可
   def customer_params
     params.require(:customer).permit(:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:phone_number,:email,:is_active)
   end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,40 +1,31 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+   #サインインする前にconfigure_sign_in_paramsメソッド実行
    before_action :configure_sign_in_params, only: [:create]
+   #サインインする前にcustomer_state実行
    before_action :customer_state,only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  #サインイン後のリダイレクト先を指定してflashメッセージを表示
   def after_sign_in_path_for(resource)
    flash[:notice] = "ようこそ！ながのCAKEへ！"
       root_path
   end
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
   private
+
   def customer_state
-   customer = Customer.find_by(email: params[:customer][:email])
-   return if customer.nil?
-   return unless customer.valid_password?(params[:customer][:password])# 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
-   unless customer.is_active == true
+   customer = Customer.find_by(email: params[:customer][:email])#customerモデルに保存されてるデータの中から入力フォームから送られたemailに紐づくcustomerの情報を取得する
+   return if customer.nil?#取得してきたcustomerのデータが存在するか判定、ないならメソッド終了
+   return unless customer.valid_password?(params[:customer][:password])#取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
+   unless customer.is_active == true#会員ステータスがtrueでないなら新規登録画面に戻る
      flash[:alert] = "すでに退会しています。"
       redirect_to new_customer_registration_path
    end
   end
 
    protected
-
-  # If you have extra params to permit, append them to the sanitizer.
+  #サインインするときのカラムの許可
    def configure_sign_in_params
      devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
    end
