ae2e8af01af5e713f5f3373de123d0cd6ac8bf68
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-23 07:32:16 +0000

    N+1問題とその他

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,14 +3,14 @@ class Public::HomesController < ApplicationController
     # ジャンル取得
     @genres = Genre.all
     # 販売中の全商品から5個ランダムで取得
-    @items = Item.where(is_active: true).order("RANDOM()").limit(5)
+    @items = Item.with_attached_image.where(is_active: true).order("RANDOM()").limit(5)
     # 新着商品を6個取得
-    @newitems = Item.where(is_active: true).order(created_at: :desc).limit(6)
+    @newitems = Item.with_attached_image.where(is_active: true).order(created_at: :desc).limit(6)
   end
 
   def about
-    @items1 = Item.where(is_active: true).order(created_at: :desc).limit(10)
-    @items2 = Item.where(is_active: true).order(created_at: :asc).limit(10)
+    @items1 = Item.with_attached_image.where(is_active: true).order(created_at: :desc).limit(10)
+    @items2 = Item.with_attached_image.where(is_active: true).order(created_at: :asc).limit(10)
 
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,16 +1,16 @@
 class Public::ItemsController < ApplicationController
   def index
     if params[:genre_id].nil? #genre_idが無かったら(全商品一覧ページの場合)
-      @items = Item.all.page(params[:page]).per(8) #item全件取得し、1ページあたり8個ずつ表示
+      @items = Item.with_attached_image.page(params[:page]).per(8) #item全件取得し、1ページあたり8個ずつ表示
     else
       @genre = Genre.find(params[:genre_id].to_i)# genre_idがあれば(ジャンル別商品一覧ページの場合)
-      @items = Item.where(genre_id: params[:genre_id].to_i).page(params[:page]).per(8) #genre_idが一致するitem全件取得し、1ページあたり8個ずつ表示
+      @items = Item.with_attached_image.where(genre_id: params[:genre_id].to_i).page(params[:page]).per(8) #genre_idが一致するitem全件取得し、1ページあたり8個ずつ表示
     end
     @genres = Genre.all #genre検索フォーム表示
   end
 
   def show
-    @item = Item.find(params[:id]) #詳細表示したいitemのデータ取得
+    @item = Item.with_attached_image.find(params[:id]) #詳細表示したいitemのデータ取得
     @genres = Genre.all #genre検索フォーム表示
   end
 
