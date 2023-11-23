fb9dafeab7a0cdef1e137e902b0eb59cb68bd318
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-23 18:55:46 +0000

    seedいじり、admin/items/indexいじり

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
   15.times do |n|
     Customer.create!(
       last_name: "試験#{n + 1}",
@@ -24,13 +17,13 @@
   Genre.create(name: "焼き菓子")
   Genre.create(name: "キャンディ")
 
-  10.times do |n|
-    Item.create!(
+  32.times do |n|
+    item = Item.create!(
       genre_id: 1,
-      name: "商品#{n + 1}",
+      name: "商品名#{n + 1}",
       description: "商品説明#{n + 1}",
-      excluding_tax_price: 2000
-      )
+      excluding_tax_price: rand(500..1500)
+    )
   end
 
   Admin.create!(
