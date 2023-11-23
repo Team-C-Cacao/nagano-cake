acc09b28aface12e6ff386c4badcf4033ed54d4a
Tsmore <143774064+Tsmore@users.noreply.github.com>
2023-11-23 10:44:01 +0000

    カートの削除系、エラーはalertに成功はnoticeに変更,

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -20,7 +20,7 @@ class Public::CartItemsController < ApplicationController
     if @cart_item.update(cart_item_params)
       redirect_to cart_items_path, notice: "#{@cart_item.item.name}の数量を#{@cart_item.amount}個に更新しました。"
     else
-      render 'cart_items/index', notice: "数量の更新に失敗しました。"
+      render 'cart_items/index', alert: "数量の更新に失敗しました。"
     end
   end
 
@@ -28,15 +28,15 @@ class Public::CartItemsController < ApplicationController
     @cart_item = CartItem.find(params[:id])
     item_name = @cart_item.item.name
     if @cart_item.destroy
-      redirect_to cart_items_path, notice: "#{item_name}を削除しました。"
+      redirect_to cart_items_path, alert: "#{item_name}を削除しました。"
     else
-      render 'cart_items/index', notice: "#{item_name}の削除に失敗しました。"
+      render 'cart_items/index', alert: "#{item_name}の削除に失敗しました。"
     end
   end
 
   def destroy_all
     CartItem.where(customer_id: current_customer.id).destroy_all
-    redirect_to cart_items_path, notice: "カート内商品をすべて削除しました。"
+    redirect_to cart_items_path, alert: "カート内商品をすべて削除しました。"
   end
 
 
@@ -48,7 +48,7 @@ class Public::CartItemsController < ApplicationController
   end
 
   def redirect_to_failure
-    redirect_to item_path(cart_item_params[:item_id]), notice: "数量を選択してください。"
+    redirect_to item_path(cart_item_params[:item_id]), alert: "数量を選択してください。"
   end
 
   def cart_item_params
