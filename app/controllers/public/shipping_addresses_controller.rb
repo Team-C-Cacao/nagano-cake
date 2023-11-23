8c0334123715a37b833806893512466aaffc9f86
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2023-11-23 02:35:48 +0000

    コメントアウト（記述の意味）

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,33 +1,33 @@
 class Public::ShippingAddressesController < ApplicationController
-  def index
-    @shipping_addresses = ShippingAddress.where(customer: current_customer)
+  def index　#一覧表示ページに配送先情報を表示するだけでなく、新しい配送先を入力するフォームを表示するようにしている。
+    @shipping_addresses = ShippingAddress.where(customer: current_customer)#customerカラムがcurrent_customerと一致するものを選択する
     @shipping_address = ShippingAddress.new
   end
 
-  def create
+  def create  #新しい配送先情報を作成し、データベースに保存する
       @shipping_address = ShippingAddress.new(shipping_address_params)
-      @shipping_address.customer_id = current_customer.id
+      @shipping_address.customer_id = current_customer.id#ShippingAdressオブジェクトに現在のユーザー('cuurent_customer')のIDを関連付けする。それにより新しい配送先情報がどのユーザーに関連しているか示す。
       if @shipping_address.save
-        redirect_to request.referer, notice: "新規配送先を登録しました。"
-      else
-        @shipping_addresses = ShippingAddress.all
-        render 'index'
+        redirect_to request.referer, notice: "新規配送先を登録しました。" #リダイレクトして元々のページに戻り成功メッセージを表示
+      else #失敗したら
+        @shipping_addresses = ShippingAddress.all #全ての配送先情報を取得して
+        render 'index' #ビューを表示し、エラーを伝える
       end
     end
 
-  def destroy
+  def destroy #特定の配送先情報を削除して、一覧ページにリダイレクトする
     @shipping_address = ShippingAddress.find(params[:id])
     @shipping_address.destroy
     flash.now[:alert] = "配送先を削除しました"
     redirect_to shipping_addresses_path
   end
 
-  def edit
+  def edit #対象となる配送先情報がデータベースから取得、それを編集するためのフォームを表示
     @shipping_address = ShippingAddress.find(params[:id])
   end
 
 
-  def update
+  def update  #特定の配送先情報を更新、表示する
     @shipping_address = ShippingAddress.find(params[:id])
     if @shipping_address.update(shipping_address_params)
       redirect_to shipping_addresses_path, notice: "配達先を変更しました。"
@@ -38,7 +38,7 @@ class Public::ShippingAddressesController < ApplicationController
 
   private
 
-  def shipping_address_params
+  def shipping_address_params #
     params.require(:shipping_address).permit(:postal_code, :shipping_address, :name)
   end
 
