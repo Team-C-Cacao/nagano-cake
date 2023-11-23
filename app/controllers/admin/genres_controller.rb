9e2c835f805781d60a35e4bf16596e612ab9d08b
HinakoKobayashi <144582159+HinakoKobayashi@users.noreply.github.com>
2023-11-23 00:43:30 +0000

    コントローラーコメントアウト記述

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,23 +1,23 @@
 class Admin::GenresController < ApplicationController
   def index
-    @genre = Genre.new
-    @genres = Genre.all
+    @genre = Genre.new #genre新規登録用の箱
+    @genres = Genre.all #genre全件取得
   end
 
   def create
-    @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to admin_genres_path
+    @genre = Genre.new(genre_params) #genre新規登録データ取得
+    @genre.save #保存
+    redirect_to admin_genres_path #genre一覧に戻る
   end
 
   def edit
-    @genre = Genre.find(params[:id])
+    @genre = Genre.find(params[:id]) #編集したいgenreのデータ取得
   end
 
   def update
-    @genre = Genre.find(params[:id])
-    @genre.update(genre_params)
-    redirect_to admin_genres_path
+    @genre = Genre.find(params[:id]) #更新したいgenreのデータ取得
+    @genre.update(genre_params) #対象データ更新
+    redirect_to admin_genres_path #genre一覧に戻る
   end
 
   private
