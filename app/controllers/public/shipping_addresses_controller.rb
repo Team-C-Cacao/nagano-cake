41c2bbbf2f12936407804d02ebab321b2068383d
xxkrxx <143770189+xxkrxx@users.noreply.github.com>
2023-11-23 04:54:07 +0000

    修正

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,19 +1,19 @@
 class Public::ShippingAddressesController < ApplicationController
-  def index　#一覧表示ページに配送先情報を表示するだけでなく、新しい配送先を入力するフォームを表示するようにしている。
-    @shipping_addresses = ShippingAddress.where(customer: current_customer)#customerカラムがcurrent_customerと一致するものを選択する
+  def index #一覧表示ページに配送先情報を表示するだけでなく、新しい配送先を入力するフォームを表示するようにしている。
+    @shipping_addresses = ShippingAddress.where(customer: current_customer) #customerカラムがcurrent_customerと一致するものを選択する
     @shipping_address = ShippingAddress.new
   end
 
   def create  #新しい配送先情報を作成し、データベースに保存する
       @shipping_address = ShippingAddress.new(shipping_address_params)
-      @shipping_address.customer_id = current_customer.id#ShippingAdressオブジェクトに現在のユーザー('cuurent_customer')のIDを関連付けする。それにより新しい配送先情報がどのユーザーに関連しているか示す。
+      @shipping_address.customer_id = current_customer.id #ShippingAdressオブジェクトに現在のユーザー('cuurent_customer')のIDを関連付けする。それにより新しい配送先情報がどのユーザーに関連しているか示す。
       if @shipping_address.save
         redirect_to request.referer, notice: "新規配送先を登録しました。" #リダイレクトして元々のページに戻り成功メッセージを表示
       else #失敗したら
-        @shipping_addresses = ShippingAddress.all #全ての配送先情報を取得して
+        @shipping_addresses = ShippingAddress.where(customer: current_customer) #
         render 'index' #ビューを表示し、エラーを伝える
       end
-    end
+  end
 
   def destroy #特定の配送先情報を削除して、一覧ページにリダイレクトする
     @shipping_address = ShippingAddress.find(params[:id])
@@ -38,7 +38,7 @@ class Public::ShippingAddressesController < ApplicationController
 
   private
 
-  def shipping_address_params #
+  def shipping_address_params 
     params.require(:shipping_address).permit(:postal_code, :shipping_address, :name)
   end
 
