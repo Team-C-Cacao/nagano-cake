8c564601be962729c3035a66cf64c7800dda1ab9
PikkaStar <yokotaka1226@gmail.com>
2023-11-24 02:41:07 +0000

    flash/alert

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -7,6 +7,12 @@ class Admin::SessionsController < Devise::SessionsController
   # def new
   #   super
   # end
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message! :alert, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
 
   # POST /resource/sign_in
   # def create
